840a1dd209a5ae470a8bbf0895f95e4a731a28fc
Ryotaro Tsuda <utautattaro@users.noreply.github.com>
2023-11-30 17:34:35 +0900

    edit bannar

--- packages/core/src/ui/editor/default-content.tsx
+++ packages/core/src/ui/editor/default-content.tsx
@@ -19,7 +19,7 @@ export const defaultEditorContent = {
         {
           "type": "image",
           "attrs": {
-            "src": "https://public.blob.vercel-storage.com/pJrjXbdONOnAeZAZ/banner-2wQk82qTwyVgvlhTW21GIkWgqPGD2C.png",
+            "src": "https://i.imgur.com/znF5Tec.png",
             "alt": "banner.png",
             "title": "banner.png",
             "width": null,
